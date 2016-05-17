263a528d4a4588ce85cf442aa90f843e6d377d24
bhoff <bruce.hoff@sagebase.org>
2016-05-17 15:35:53 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu
+ADD train.sh /train.sh
+ADD test.sh /test.sh
+
---
+++ train.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "this content represents some model state to be retrieved" > /foo.txt
+echo this is the output of the training script
+ls -al /trainingData
